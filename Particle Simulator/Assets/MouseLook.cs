d980978a422be07bfe0ef6a7c88ad0bc4a53e914
maver222 <88016010+maver222@users.noreply.github.com>
2021-10-18 13:46:03 +0800

    Update MouseLook.cs

--- Particle Simulator/Assets/MouseLook.cs
+++ Particle Simulator/Assets/MouseLook.cs
@@ -28,8 +28,10 @@ public class MouseLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         /*Rotates independant of the frame rate*/
         xRotation -= mouseY;
+        /*Clamp rotation so we cant over rotate and go behind the player*/
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        /*Reference for local camera*/
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
