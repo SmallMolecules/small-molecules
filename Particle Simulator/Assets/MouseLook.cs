6d0d84aac11e419b5e82128d504274a94c5f4aec
maver222 <88016010+maver222@users.noreply.github.com>
2021-10-18 13:49:02 +0800

    Create MouseLook.cs

---
+++ Particle Simulator/Assets/MouseLook.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/** @brief Mouse Look
+
+    This class manages a first person field of view inside the simulation. 
+    @author Matthew Vereker
+    @date September 2021
+    */
+public class MouseLook : MonoBehaviour
+{
+    /*Sets the mouse sensitivity to a default value and controls the speed of the mouse*/
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+    	Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    	 /*Will get the input of the mouse coordinates in the simulation*/
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        /*Rotates independant of the frame rate*/
+        xRotation -= mouseY;
+        /*Clamp rotation so we cant over rotate and go behind the player*/
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        /*Reference for local camera*/
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
+ 
